a66601f58c04b80b29141fe7543d4fca955c9786
李俊翰, Lee Jun-Han <s411178060@gm.ntpu.edu.tw>
2024-11-02 04:26:27 +0800

    Update Bangkok.py

--- Bangkok.py
+++ Bangkok.py
@@ -13,8 +13,7 @@ import time
 import os
 import csv
 import re
-import json
-import logging
+
 
 # 獲取 Discord Webhook URL
 WEBHOOK_URL = "https://discord.com/api/webhooks/1301474261176942603/RJrSYYyNLbETUQ5KN5OWmhKagk1ahY3bCg_dxKoT5tpe-f55jDfp1BznAxiN4Dx5rQ8u"
